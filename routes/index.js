6db505d72615c79f80812d48e7cef1b9795a01cc
danny <livebetterlife0820@gmail.com>
2017-09-02 03:18:00 -0400

    [feature]: change file structure under directory 'views'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Express', project: 'RESTful_API_Practice'}); //use index.ejs
+  res.render('pages/index', {title: 'Express', project: 'RESTful_API_Practice'}); //use index.ejs
 });
 
 module.exports = router;
